06afa7e0eaa56393ec404b71cedf34d293b45415
Alec Hannaford <alec322@gmail.com>
2023-01-31 19:10:53 -0800

    user can purchase and use a battery lawnmower

--- landscape.py
+++ landscape.py
@@ -9,13 +9,13 @@ inventory = {
 }
 
 def lvl1():
+    global money
     choice = input("""
                    A - Chew Grass
                    B - Buy scissors
                    What do you do? > 
                    """)
     if choice.upper() == 'A':
-        global money
         money += 1
         print(f"{money}$ in the bank!")
     if choice.upper() == "B":
@@ -27,6 +27,7 @@ def lvl1():
             mop.append(1)
 
 def lvl2():
+    global money
     choice = input(
         """
         A - Cut Grass w Scissors
@@ -35,14 +36,41 @@ def lvl2():
         """
         )
     if choice.upper() == "A":
-        global money
         money += 5
         print(f"{money}$ in the bank!")
+    if choice.upper() == "B":
+        if money < 25:
+            print("Come back when u have 25 quid")
+        else:
+            money -= 25
+            print('PURCHASED A PUSH LAWNMOWER')
+            mop.append(1)
 
+def lvl3():
+    global money
+    choice = input(
+        """
+        A - Cut Grass w push lawnmower
+        B - Buy electic lawnmower
+        What do you do? >
+        """
+        )
+    if choice.upper() == "A":
+        money += 50
+        print(f"{money}$ in the bank!")
+    if choice.upper() == "B":
+        if money < 25:
+            print("Come back when u have 250 quid")
+        else:
+            money -= 250
+            print('PURCHASED A Battery LAWNMOWER')
+            mop.append(1)
 
 def engine():
     while len(mop) == 0:
         lvl1()
     while len(mop) == 1:
         lvl2()
+    while len(mop) == 2:
+        lvl3()
 engine()
