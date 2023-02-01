3e5d36e278483c04ce60e649a65d8252699e1b1e
Alec Hannaford <alec322@gmail.com>
2023-01-31 18:54:57 -0800

    user can buy scissors and cut grass w them

--- landscape.py
+++ landscape.py
@@ -22,12 +22,27 @@ def lvl1():
         if money < 5:
             print('U BROKE')
         else:
+            money -= 5
             print("PURCHASED SCISSORS")
             mop.append(1)
+
+def lvl2():
+    choice = input(
+        """
+        A - Cut Grass w Scissors
+        B - Buy push lawnmower
+        What do you do? >
+        """
+        )
+    if choice.upper() == "A":
+        global money
+        money += 5
+        print(f"{money}$ in the bank!")
 
 
 def engine():
     while len(mop) == 0:
         lvl1()
-        
+    while len(mop) == 1:
+        lvl2()
 engine()
